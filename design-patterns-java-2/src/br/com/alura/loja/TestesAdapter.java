8d4b2bfae948e748767311327d05ff605292e729
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2023-08-11 16:10:13 -0300

    feat: init

--- design-patterns-java-2/src/br/com/alura/loja/TestesAdapter.java
+++ design-patterns-java-2/src/br/com/alura/loja/TestesAdapter.java
@@ -3,13 +3,21 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.http.ApacheHttpAdapter;
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
 import br.com.alura.loja.orcamento.RegistroOrcamento;
 
 public class TestesAdapter {
 
 	public static void main(String[] args) {
-		Orcamento orcamento = new Orcamento(BigDecimal.TEN, 1);
+
+		int i = 010;
+		int j = 07;
+
+		System.out.println(i);
+		System.out.println(j);
+		Orcamento orcamento = new Orcamento();
+		orcamento.adicionarItem(new ItemOrcamento(BigDecimal.TEN));
 		orcamento.aprovar();
 		orcamento.finalizar();
 
